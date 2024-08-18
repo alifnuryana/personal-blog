d41ee0d6a261d07ddc9b6d65184d1f59ccb84418
Alif Nuryana <nuryana.alif@hotmail.com>
2024-08-18 06:17:31 +0000

    use analytic and speed insight vercel

--- astro.config.ts
+++ astro.config.ts
@@ -3,8 +3,13 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import UnoCSS from 'unocss/astro'
 import vue from '@astrojs/vue'
+import vercel from '@astrojs/vercel/serverless'
 
 export default defineConfig({
+  output: 'hybrid',
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
   site: 'https://alifnuryana.netlify.app/',
   server: {
     port: 1977,
