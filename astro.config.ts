b5a06f6f04f0230b72b28e59a3077c3e643948ac
Alif Nuryana <nuryana.alif@hotmail.com>
2024-08-18 15:06:41 +0000

    add turnslate and project

--- astro.config.ts
+++ astro.config.ts
@@ -3,20 +3,20 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import UnoCSS from 'unocss/astro'
 import vue from '@astrojs/vue'
+import turnstile from 'astro-turnstile'
 
+// https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
   site: 'https://alifnuryana.my.id/',
   server: {
     port: 1977,
   },
-  integrations: [
-    mdx(),
-    sitemap(),
-    UnoCSS({
-      injectReset: true,
-    }),
-    vue(),
-  ],
+  integrations: [mdx(), sitemap(), UnoCSS({
+    injectReset: true,
+  }), vue(), turnstile({
+    siteKey: '0x4AAAAAAAhhSyGB5ENmU4PZ',
+  })],
   markdown: {
     shikiConfig: {
       themes: {
--- src/pages/projects/data.ts
+++ src/pages/projects/data.ts
@@ -1,5 +1,10 @@
 import type { ProjectData } from '@/types'
 
 export const projectData: ProjectData = [
-  //
+  {
+    title: '2024',
+    projects: [
+      { text: 'PT Hajjahtini Berkah Jaya', href: 'https://hajjahtiniberkahjaya.com', description: 'Company Profile PT Hajjahtini Berkah Jaya', icon: 'i-carbon-campsite' },
+    ],
+  },
 ]
